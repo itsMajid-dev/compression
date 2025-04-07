9c4d25aaa7fc870a4225d768193273633120dc18
Majid <itsMajid.dev@gmail.com>
2025-04-07 10:06:43 +0330

    convert_to_code method

--- main.py
+++ main.py
@@ -5,4 +5,22 @@ class Encoding:
     def __init__(self , data):
         self.data = data
         self.__size = 0 
-        
+
+    def convert_to_code(self):
+        output = []  
+        tabel = num.partions
+        next_code = num.max_code
+        char = ''
+        for c in self.data:
+            substring = char+c
+            if substring in tabel:
+                char=substring
+            else:
+                output.append(tabel[char])
+                tabel[substring]=next_code
+                next_code+=1
+                char=c
+
+
+o = Encoding('abcab')
+o.convert_to_code()
